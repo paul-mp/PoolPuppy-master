018c8fafbe6a6ea9cbf6bfcb4929faff509a61cf
Paul <pavel.maslov01@gmail.com>
2022-10-24 13:02:19 -0400

    Ver 7.0

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const app = new express();
 const server = http.createServer(app);
 const port = process.env.port || 80;
 
-app.use(__dirname + '/src');
+app.use(express.static(__dirname + '/src'));
 
 app.get('/', (req, res) => {
-    res.sendFile('/src/main/index.html');
+    res.sendFile(__dirname + '/src/main/main.html');
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/src/main/index.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/src/main/index.html');
 });
 
 server.listen(port, () => console.log('Listening to '+ port)); 
